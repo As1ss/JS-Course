a9885e04256c5452fb2b53898e9758a210127b06
As1ss <alexis.lopbri@educa.jcyl.es>
2024-03-11 09:43:36 +0100

    Ejercicios añadidos

---
+++ ultimatejs/04-ejercicios/01.js
@@ -0,0 +1,21 @@
+function obtenerNumMayor(a,b){
+    if(a>b){
+        return a;
+    }
+    else if(b>a){
+        return b;
+    }
+    else{
+        console.log("Son iguales");
+    }
+    /*
+    También se podría retornar el resultado de una expresión ternaria pero no obtenemos el resultado
+    si son iguales
+    */
+
+    //return (a,b) ? a: b;
+}
+
+let resultado = obtenerNumMayor(10,5);
+
+console.log(resultado);
